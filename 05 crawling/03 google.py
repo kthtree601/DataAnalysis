39f8c2b9df5f3c6a3c5ff05133361650a40740ae
kthtree601 <kthtree601@naver.com>
2021-06-25 13:57:44 +0900

    first commit

---
+++ 05 crawling/03 google.py
@@ -0,0 +1,24 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome('./chromedriver')
+driver.get('http://www.google.com')
+time.sleep(1)
+
+search_box = driver.find_element_by_css_selector('.gLFyf.gsfi')
+search_box.send_keys('ChromeDriver')
+search_box.send_keys(Keys.ENTER)  #  Keys.RETURN 도 상관없음
+time.sleep(1)
+
+divs = driver.find_element_by_css_selector('div#rso>div.g')
+for div in divs:
+    try:
+
+    title = div.find_element_by_css_selector('.LC20lb.DKV0Md').text
+    content = div.find_element_by_css_selector('.aCOpRe').text
+    print(title)
+    print(content)
+    print('===============================')
+    except:
+        pass
